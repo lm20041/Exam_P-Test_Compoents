c51f2ede07e5c5eca31547f6efd8f517ddcd43e0
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-04-25 22:24:23 +0000

    PhotoImage (test3)
    -this new version is far more simpler to calculate an image size
    first the calculations labeled down the original width, height of the image then we input width, height we want to be. cal min(resize_factor) then use that to cal subsample(resized_image) which gives us the image new side

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def add_label():
     label.pack()
 
 root = tk.Tk()
-root.geometry("300x200")
+root.geometry("400x300")
 
 frame = tk.Frame(root, bg="lightgrey", borderwidth=10, relief="ridge", padx=10, pady=10)
 frame.pack(pady=20)
@@ -17,10 +17,13 @@ frame.pack(pady=20)
 # Load the image
 original_image = PhotoImage(file="R4_3_1-main_screen.png")
 
+# Calculate the resize factor
+original_width, original_height = original_image.width(), original_image.height()
+target_width, target_height = 200, 200
+resize_factor = min(target_width / original_width, target_height / original_height)
+
 # Resize the image
-width, height = original_image.width(), original_image.height()
-resize_factor = 0.01  # Change this factor according to your needs
-resized_image = original_image.subsample(int(width * resize_factor), int(height * resize_factor))
+resized_image = original_image.subsample(int(1 / resize_factor), int(1 / resize_factor))
 
 # Create a label to display the resized image
 image_label = tk.Label(frame, image=resized_image)
